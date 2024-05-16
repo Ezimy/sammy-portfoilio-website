69ce79467179f7204f404838f4f78448f40dda63
Sammy005 <98131603+Ezimy@users.noreply.github.com>
2024-05-15 21:23:53 -0400

    file set up, navbar and footer

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,26 @@
+import { Link, NavLink } from 'react-router-dom';
+import './index.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import Initials from '../../assets/images/initials.png'
+function Navbar() {
+    return(
+        <div className='nav-bar'>
+            <Link className='logo' to='/'>
+                <img src={Initials} alt='SY.'/>
+            </Link>
+            <nav>
+                <NavLink exact="true" activeclassname="active" className="link home" to="/">
+                    <FontAwesomeIcon icon={faHome} color='#224870'/>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="link about" to="/About">
+                    <FontAwesomeIcon icon={faUser} color='#224870'/>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="link contact" to="/Contact">
+                    <FontAwesomeIcon icon={faEnvelope} color='#224870'/>
+                </NavLink>
+            </nav>
+        </div>
+    )
+}
+export default Navbar;
